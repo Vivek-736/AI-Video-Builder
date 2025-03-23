95dd15616e05399d42fe1b265e15dc76bb257af1
Vivek <vivekousik8555@gmail.com>
2025-03-23 21:28:28 +0530

    set up database and connected it with drizzle orm

---
+++ configs/schema.ts
@@ -0,0 +1,8 @@
+import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
+
+export const Users = pgTable("users", {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 255 }).notNull(),
+    email: varchar("email", { length: 255 }).notNull().unique(),
+    imageUrl: varchar("imageUrl")
+});
