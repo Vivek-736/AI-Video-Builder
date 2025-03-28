7463df8f9b5427bfb6b7e7c8659f02952bf180cf
Vivek <vivekousik8555@gmail.com>
2025-03-28 14:09:25 +0530

    added Link for the title of the website using next/link in order to redirect on click to home page

--- components/Dashboard/Header.tsx
+++ components/Dashboard/Header.tsx
@@ -3,17 +3,20 @@ import React from "react";
 import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
 import { ModeToggle } from "../mode-toggle";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="p-3 px-5 flex justify-between items-center border-b-2 border-gray-400 dark:border-gray-800 bg-slate-200 dark:bg-slate-900">
       <div className="md:text-4xl text-2xl flex items-center font-medium">
-        <div className="flex gap-3 items-center">
-          <Image src="/logo.png" alt="logo" width={42} height={42} />
-          <div>
-            Vid<span className="text-blue-500 font-bold">Genius</span>
+        <Link href={"/"}>
+          <div className="flex gap-3 items-center cursor-pointer">
+            <Image src="/logo.png" alt="logo" width={42} height={42} />
+            <div>
+              Vid<span className="text-blue-500 font-bold">Genius</span>
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
       <div className="flex gap-3 items-center">
         <ModeToggle />
