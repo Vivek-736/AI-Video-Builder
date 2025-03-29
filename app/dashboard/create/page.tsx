4c251e3afc91f3e971ff7df3d2b49db864224d1f
Vivek <vivekousik8555@gmail.com>
2025-03-29 21:56:22 +0530

    select topic component created and implemented the UI for create

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import SelectTopic from '@/components/Dashboard/SelectTopic'
 
 const page = () => {
     return (
         <div className='md:px-20 p-6'>
             <h2 className='font-bold text-2xl md:text-4xl dark:text-indigo-400 text-indigo-600 text-center'>
-                Create something afresh
+                Create Anything!
             </h2>
-            <div>
-                
+            <div className='mt-10 p-10 shadow-lg shadow-gray-700 dark:shadow-gray-300'>
+                <SelectTopic />
             </div>
         </div>
     )
---
+++ components/Dashboard/SelectTopic.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import React, { useState } from "react";
+import { ArrowBigRight } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "../ui/textarea";
+
+const SelectTopic = () => {
+  const options = [
+    "Motivational Story",
+    "Funny Story",
+    "Daunting Story",
+    "Technical Stuff",
+    "Random Video",
+    "Custom Prompt",
+  ];
+  const [selectdOption, setSelectedOption] = useState<string | undefined>();
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-row justify-between items-center">
+        <div className="space-y-2">
+          <h2 className="font-bold text-2xl text-indigo-600 dark:text-indigo-400">
+            Content
+          </h2>
+          <div>
+            <p className="text-gray-500 dark:text-gray-300 flex flex-row gap-x-2 items-center">
+              Select your video core{" "}
+              <ArrowBigRight className="text-black dark:text-white" />
+            </p>
+          </div>
+        </div>
+        <div>
+          <Select onValueChange={(value) => setSelectedOption(value)}>
+            <SelectTrigger className="w-full mt-2 p-6 text-lg cursor-pointer">
+              <SelectValue placeholder="Video Theme" />
+            </SelectTrigger>
+            <SelectContent>
+              {options.map((item, index) => (
+                <SelectItem key={index} value={item}>
+                  {item}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+      {selectdOption === "Custom Prompt" && (
+        <Textarea className="mt-4 border-2" placeholder="Write Your Custom Prompt" />
+      )}
+    </div>
+  );
+};
+
+export default SelectTopic;
