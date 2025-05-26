448f7db649a02ba1eba9e4da2f2819a3500a0e02
Vivek <vivekousik8555@gmail.com>
2025-05-26 13:05:28 +0530

    added the back button to smaller devices and removed the text

--- components/Dashboard/BackButton.tsx
+++ components/Dashboard/BackButton.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
 import React from "react";
@@ -11,7 +10,6 @@ const BackButton = () => (
       className="flex items-center text-indigo-600 dark:text-indigo-400"
     >
       <ArrowLeft className="w-5 h-5 mr-2" />
-      Back
     </Link>
   </div>
 );
